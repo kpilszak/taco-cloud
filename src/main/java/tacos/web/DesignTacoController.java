7fccca9fd1b65ca956dde4ca365b882ecdaaa274
kpilszak <klaudia.pilszak@gmail.com>
2020-04-23 16:37:12 +0200

    feat: Spring Security Add inMemoryAuthentication

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,54 +1,127 @@
 package tacos.web;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import lombok.extern.slf4j.Slf4j;
-import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
-@Slf4j
+//tag::injectingDesignRepository[]
+//tag::injectingIngredientRepository[]
 @Controller
 @RequestMapping("/design")
+//end::injectingIngredientRepository[]
+@SessionAttributes("order")
+//tag::injectingIngredientRepository[]
 public class DesignTacoController {
 
-	@GetMapping
-	public String showDesignForm(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "wheat", Type.WRAP),
-				new Ingredient("COTO", "maize", Type.WRAP),
-				new Ingredient("GRBF", "ground beef", Type.PROTEIN),
-				new Ingredient("CARN", "meat pieces", Type.PROTEIN),
-				new Ingredient("TMTO", "diced tomatoes", Type.VEGGIES),
-				new Ingredient("LETC", "lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "cheddar", Type.CHEESE),
-				new Ingredient("JACK", "Monterey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "salsa", Type.SAUCE),
-				new Ingredient("SRCR", "sour cream", Type.SAUCE)
-				);
-		
-		Type[] types = Ingredient.Type.values();
-		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-		}
-		model.addAttribute("design", new Taco());
-		return "design";
-	}
-	
-	@PostMapping
-	public String processDesign(Design design) {
-		log.info("Processing taco: " + design);
-		return "redirect:/orders/current";
-	}
+  private final IngredientRepository ingredientRepo;
+
+  //end::injectingIngredientRepository[]
+  private TacoRepository tacoRepo;
+
+  //end::injectingDesignRepository[]
+  /*
+  //tag::injectingIngredientRepository[]
+  public DesignTacoController(IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
+  //end::injectingIngredientRepository[]
+   */
+  //tag::injectingDesignRepository[]
+
+  @Autowired
+  public DesignTacoController(
+        IngredientRepository ingredientRepo,
+        TacoRepository tacoRepo) {
+    this.ingredientRepo = ingredientRepo;
+    this.tacoRepo = tacoRepo;
+  }
+
+  @ModelAttribute(name = "order")
+  public Order order() {
+    return new Order();
+  }
+
+  @ModelAttribute(name = "design")
+  public Taco design() {
+    return new Taco();
+  }
+
+  //end::injectingDesignRepository[]
+
+  //tag::injectingIngredientRepository[]
+
+  @GetMapping
+  public String showDesignForm(Model model) {
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
+    Type[] types = Ingredient.Type.values();
+    for (Type type : types) {
+      model.addAttribute(type.toString().toLowerCase(),
+          filterByType(ingredients, type));
+    }
+
+    return "design";
+  }
+  //end::injectingIngredientRepository[]
+
+//tag::injectingDesignRepository[]
+  @PostMapping
+  public String processDesign(
+      @Valid Taco taco, Errors errors,
+      @ModelAttribute Order order) {
+
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
+    Taco saved = tacoRepo.save(taco);
+    order.addDesign(saved);
+
+    return "redirect:/orders/current";
+  }
+
+//end::injectingDesignRepository[]
+
+  private List<Ingredient> filterByType(
+      List<Ingredient> ingredients, Type type) {
+    return ingredients
+              .stream()
+              .filter(x -> x.getType().equals(type))
+              .collect(Collectors.toList());
+  }
+
+  /*
+  //tag::injectingDesignRepository[]
+  //tag::injectingIngredientRepository[]
+
+   ...
+  //end::injectingIngredientRepository[]
+  //end::injectingDesignRepository[]
+  */
+
+//tag::injectingDesignRepository[]
+//tag::injectingIngredientRepository[]
+
 }
+//end::injectingIngredientRepository[]
+//end::injectingDesignRepository[]
