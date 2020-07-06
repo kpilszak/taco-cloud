18cdb42f91c0a5bea5fc9a50d7ca149dbbdf10c4
kpilszak <klaudia.pilszak@gmail.com>
2020-07-06 20:32:06 +0200

    feat: Add user registration

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,32 +1,30 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	DataSource dataSource;
-	
+	private UserRepositoryUserDetailsService userRepositoryUserDetailsService;
+
+	@Bean
+    public PasswordEncoder encoder() {
+	    return new BCryptPasswordEncoder();
+    }
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.ldapAuthentication()
-				.userSearchBase("ou=people")
-				.userSearchFilter("(uid={0}")
-				.groupSearchBase("ou=groups")
-				.groupSearchFilter("member={0}")
-				.passwordCompare()
-				.passwordEncoder(new BCryptPasswordEncoder())
-				.passwordAttribute("passcode");
+			.userDetailsService(userRepositoryUserDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
